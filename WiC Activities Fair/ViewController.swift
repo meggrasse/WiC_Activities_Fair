6d8e40943e4c2837d44ed777ac8096f2a9966b9a
Meg Grasse <meggrasse@gmail.com>
2017-01-19 14:36:54 -0600

    Initial project.

---
+++ WiC Activities Fair/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WiC Activities Fair
+//
+//  Created by Meg Grasse on 9/13/16.
+//  Copyright © 2016 Meg Grasse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red;
+        UIView.animate(withDuration: 0.1, delay: 0.0, options:[UIViewAnimationOptions.repeat, UIViewAnimationOptions.autoreverse], animations: {
+            self.view.backgroundColor = UIColor.red
+                }, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
